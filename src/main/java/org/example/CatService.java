05578ad4fe171613272a304fda1c01a080769cc0
Ricardo Patino <rpatino12@outlook.com>
2023-02-08 17:18:15 -0500

    Here we get the data from the cat api to Java and parse the response to the Cat class, so we can manipulate the cat attributes like 'id', 'url'

--- src/main/java/org/example/CatService.java
+++ src/main/java/org/example/CatService.java
@@ -1,6 +1,61 @@
 package org.example;
 
+import java.io.IOException;
+import okhttp3.*;
+import com.google.gson.Gson;
+
+import java.awt.*;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class CatService {
-    public static void showCats(){
+    public static void showCats() throws IOException {
+
+        // First we get the data from the API, the following code was extracted from Postman (code snippet)
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        // Using the GET method, we request create the request
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").method("GET", null).build();
+        // Now we execute the request and store the response in the Response object
+        // And we have to the method throw an IOException, because this method is connecting with things external to the app
+        Response response = client.newCall(request).execute();
+
+        // Now we store the response with the String type
+        String jsonResponse = response.body().string();
+        // And we have to format the string, so we can erase the square brackets that come with the response by default
+        jsonResponse = jsonResponse.substring(1, jsonResponse.length() - 1);
+
+
+        // Finally we create a Gson object, to convert the API response to a Cat object
+        Gson gson = new Gson();
+        // Here we parse the response to the Cat type, so this Cat object has the attributes "id", "url", "image"
+        Cat cat = gson.fromJson(jsonResponse, Cat.class);
+
+        // Now we need to show the cat image
+        Image image = null;
+        try {
+            // We get the url of the image from the getUrl method of the cat object
+            URL url = new URL(cat.getUrl());
+            // Now we read the image url to load the image in the screen
+            image = ImageIO.read(url);
+
+            // Because the swing.JOptionPane class receives an image icon object we cast the image to an image icon object
+            ImageIcon catIcon = new ImageIcon(image);
+
+            // The extra step is resize the image in the case that the result image is too large
+            if (catIcon.getIconWidth() > 800){
+                // With the Image class methods we can scale the image, so we cast again the catIcon
+                Image catImage = catIcon.getImage();
+                Image resize = catImage.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+
+                // Finally we have the catIcon resized, if it was needed
+                catIcon = new ImageIcon(resize);
+            }
+
+        }
+        // We have to add an IOException
+        catch (IOException e){
+            System.err.println(e);
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String[] buttons = {"Show cats", "Show favorites", "Exit"};
         String option;
 
